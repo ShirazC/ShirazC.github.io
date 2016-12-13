fa2566f55a124652d9c5717c3cff09e0c285052b
Shiraz Chokshi <shirazchokshi@Shirazs-MBP.fios-router.home>
2016-12-12 21:14:25 -0500

    fixed js settings

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,12 @@
 function setup() { 
-  createCanvas(400, 400);
+  createCanvas(1000, 1000, WEBGL);
 } 
 
 function draw() { 
   background(220);
+  fill(100, 100, 100);
+  sphere(40);
+  rotateY(0);
+
+
 }
