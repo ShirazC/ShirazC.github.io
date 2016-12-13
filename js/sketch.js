a897eb5aa614b8111c79c0b972a609cfc3cba31b
Shiraz Chokshi <shirazchokshi@Shirazs-MBP.fios-router.home>
2016-12-12 21:17:19 -0500

    added texture and changed size of sphere

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,18 @@
+
+
+var img;
+
+function preload(){
+	img = loadImage("earthmap.jpg");
+}
 function setup() { 
   createCanvas(1000, 1000, WEBGL);
 } 
 
 function draw() { 
   background(220);
-  fill(100, 100, 100);
-  sphere(40);
+  texture(img);
+  sphere(100);
   rotateY(0);
 
 
